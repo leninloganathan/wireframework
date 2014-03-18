2aa1dd99f08bc438a77600f5d987abf5ec3efb21
Reinier Ladan <reinierl@gmail.com>
2014-03-18 17:27:03 +0100

    Annotation functionality included

--- src/javascript/navigation.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-	$('.open-navigation').click(function(){
-		$('body').addClass('js-navigation-is-open');
-	});
-
-	$('.close-navigation').click(function(){
-		$('body').removeClass('js-navigation-is-open');
-	});
-});
